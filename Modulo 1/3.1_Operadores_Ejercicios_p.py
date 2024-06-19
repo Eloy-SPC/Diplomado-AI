943ce3dc6da31011b57b7fa996516df8c9b14ec1
Eloy-SPC <eloysergiomx@gmail.com>
2024-06-19 17:40:33 -0600

    Ejercicios de la tercer sesion

--- Modulo 1/3.1_Operadores_Ejercicios_p.py
+++ Modulo 1/3.1_Operadores_Ejercicios_p.py
@@ -1,5 +1,4 @@
-""""
-Escribir un programa que solicite 2 numero y luego imprima
+"""" Escribir un programa que solicite 2 numero y luego imprima
     1. La suma de los dos numeros
     2. La resta del primer nuemro menos el segundo
     3. El producto de los numeros
@@ -9,4 +8,15 @@ Escribir un programa que solicite 2 numero y luego imprima
 num1 = float(input ("ingrese el primer numero"))
 num2 = float(input ("ingrese el segundo numero"))
 
-suma = num2 + num1
+suma = num2 + num1 
+resta = num1 - num2
+producto = num2 * num1
+cubo = suma ** 3
+cociente = num1/num2
+
+print (suma)
+print (resta)
+print (producto)
+print (cubo)
+print (cociente)
+
